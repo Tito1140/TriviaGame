d482e156df824cc513c21d07cfcdc78fe1d4057f
John Tower <juan.c.torres1140@gmail.com>
2019-02-26 22:26:10 -0500

    Questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+// var number = 30;
+
+// var intervalId;
+
+// $("start").on("click", run);
+
+// function run() {
+//     clearInterval(intervalId);
+//     intervalId = setInterval(decrement, 1000);
+// }
+
+// function decrement() {
+
+//     number--;
+
+//     $("#show-number").html("<h2>" + number + "</h2>");
+
+//     if (number === 0) {
+//         stop();
+//         alert("Time Up!");
+//     }
+// }
+
+// run();
+
+var questions = [{
+    question: "1) Who is the all time leading point scorer in NBA history?",
+    choices: ["Kobe Bryant", "Lebron James", "Kareem Abdul-Jabbar", "Michael Jordan"],
+    correctAnswer: 2
+}, {
+    question: "2) What city in Australia has the highest population?",
+    choices: ["Sydney", "Perth", "Melbourne", "Brisbane"],
+    correctAnswer: 0
+}, {
+    question: "3) What blood type do you need to be a universal donor?",
+    choices: ["Type O+", "Type O-", "Type A+", "Type B-"],
+    correctAnswer: 1
+}, {
+    question: "4) What ingredient in bread causes it to rise?",
+    choices: ["Flour", "Milk", "Eggs", "Yeast"],
+    correctAnswer: 3
+}, {
+    question: "5) What is the third most abundant gas in Earth’s atmosphere?",
+    choices: ["Argon", "Helium", "Nitrogen", "Oxygen"],
+    correctAnswer: 0
+}, {
+}];
+
+var 
